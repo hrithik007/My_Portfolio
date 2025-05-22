7caee4226a3bbd0e630feba31aedd927bd66bf61
Hrithik Jaiswal <42695380+hrithik007@users.noreply.github.com>
2025-05-22 22:00:41 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,11 +8,11 @@ const Hero = () => {
           <div className="col-md-6 text-left text-white">
             <h1 className="display-4">Hi, I'm Hrithik Jaiswal</h1>
             <p className="lead">
-            Experienced software engineer with 2.7 years of expertise in Java, Spring Framework, React, and MySQL. Skilled in client
+            Experienced software engineer with 3 years of expertise in Java, Spring Framework, React, and MySQL. Skilled in client
             communication, team collaboration, problem-solving, and Agile methodologies for efficient software delivery.   </p>
             <a href="#about" className="btn btn-light btn-lg mt-3">Learn More About Me</a>
             <br />
-            <a href="https://drive.google.com/file/d/1ZEYRKIqG-xYIxRWN7Hup5jYkgDEAXBck/view?usp=sharing" className="btn btn-outline-light btn-lg mt-3 btn-primary" download>Download CV/Resume</a>
+            <a href="https://docs.google.com/document/d/1dpsrjv4qpQzgKZZUV4c2oTWTo_pvLoZO/edit?usp=sharing&ouid=115020487091127383182&rtpof=true&sd=true" className="btn btn-outline-light btn-lg mt-3 btn-primary" download>Download CV/Resume</a>
           </div>
         </div>
       </div>
